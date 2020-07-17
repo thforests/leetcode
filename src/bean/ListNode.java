23a16cf638a3b9effbc27154cfac42e4164cc032
theforests <1184569974@qq.com>
2020-07-17 17:19:29 +0800

    1_20200717

--- src/bean/ListNode.java
+++ src/bean/ListNode.java
@@ -1,18 +1,23 @@
 package bean;
 
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val; //赋值链表的值
+    public ListNode next;  // 下一个链表对象
 
     ListNode() {
     }
 
-    ListNode(int val) {
-        this.val = val;
+
+
+    ListNode(int x) {
+        val = x;
     }
+
 
     ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
+
+
 }
